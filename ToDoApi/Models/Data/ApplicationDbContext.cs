c8eac1eb5f2b9e5321e519abd8a35ae4ef374a67
artem.hlushchenko <artem.hlushchenko@cruxlab.com>
2022-02-02 12:35:10 +0200

    Add initial items
    Added SQlite database, ASP.NET Core project

---
+++ ToDoApi/Models/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoApi.Models.DomainModels;
+
+namespace ToDoApi.Models.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        { }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
---
+++ ToDoApi/Models/DomainModels/ApplicationUser.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApi.Models.DomainModels
+{
+    public class ApplicationUser
+    {
+        public int ID { get; set; } 
+
+        [Required(ErrorMessage = "Please enter a username.")]
+        [RegularExpression("(?i)^[a-z0-9 ]+$",
+            ErrorMessage = "Username may not contain special characters.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter an email address.")]
+        [Remote("CheckEmail", "Validation")]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Please enter a password.")]
+        [Compare("ConfirmPassword")]
+        [StringLength(25,
+            ErrorMessage = "Please limit your password to 25 characters.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your password.")]
+        [Display(Name = "Confirm Password")]
+        [NotMapped]
+        public string ConfirmPassword { get; set; }
+    }
+}
