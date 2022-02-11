5dd8f3db7bff39c26c09a8720224a93ee06fb8d0
artem.hlushchenko <artem.hlushchenko@cruxlab.com>
2022-02-11 11:57:53 +0200

    Add IRepository interface and Repository class

---
+++ ToDoApi/Models/Data/QueryOptions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ToDoMvc.Models.Data
+{
+    public class QueryOptions<T>
+    {
+        public Expression<Func<T, object>> OrderBy { get; set; }
+
+        private string[] includes;
+
+        public string Include
+        {
+            set
+            {
+                includes = value.Replace(" ", "").Split(',');
+            }
+        }
+        public string[] GetIncludes()
+        {
+           return includes ?? new string[0];
+        }
+    }
+}
---
+++ ToDoApi/Models/Data/Repositories/Repository.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoApi.Models.Data;
+
+namespace ToDoMvc.Models.Data.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+
+        private DbSet<T> dbset { get; set; }
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IEnumerable<T> List(QueryOptions<T> options)
+        {
+            IQueryable<T> query = BuildQuery(options);
+
+            return query.ToList();
+        }
+
+        public virtual T Get(int id)
+        {
+            return dbset.Find(id);
+        }
+
+        public virtual T Get(string id)
+        {
+            return dbset.Find(id);
+        }
+
+        public virtual T Get(QueryOptions<T> options)
+        {
+            IQueryable<T> query = BuildQuery(options);
+
+            return query.FirstOrDefault();
+        }
+
+        public void Insert(T entity)
+        {
+            dbset.Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            dbset.Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            dbset.Remove(entity);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        private IQueryable<T> BuildQuery(QueryOptions<T> options)
+        {
+            IQueryable<T> query = dbset;
+
+            foreach (string include in options.GetIncludes())
+            {
+                query = query.Include(include);
+            }
+
+            return query;
+        }
+    }
+}
